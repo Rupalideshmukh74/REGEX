a6ccc29781afe4b3d8dab260bd3cd79793da8e9f
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-19 20:34:21 +0530

    [Rupali]feat:first name validation

---
+++ InputValidation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RegularExpression
+{
+    class InputValidation
+    {
+        public void UserInput()
+        {
+            Console.WriteLine("\n First name : Starts with Capital alphabet and has minimum 3 characters..");
+            const string Regex_Name = "^[A-Z]{1}[A-Za-z]{2,}";
+
+            Console.Write(" Enter First name : ");
+            string firstName = Console.ReadLine();
+            if (Regex.IsMatch(firstName, Regex_Name) == true)
+            {
+                Console.WriteLine(" First name - Valid");
+            }
+            else
+            {
+                Console.WriteLine(" First name - Not valid");
+                UserInput();
+            }
+        }
+    }
+}
