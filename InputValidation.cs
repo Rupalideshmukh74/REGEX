5ca53767b3e6bb299b4db4cdea3cefac5b714586
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-19 21:00:24 +0530

    [Rupali]feat:min 8 Charecter Atleast

--- InputValidation.cs
+++ InputValidation.cs
@@ -8,10 +8,9 @@ namespace RegularExpression
     {
         public void UserInput()
         {
-            const string Regex_Password = "^(?=.*[a-z0-9A-Z])(?=.*[A-Z]).{8,}$";
+            const string Regex_Password = "^(?=.*[a-z0-9A-Z])(?=.*[A-Z])(?=.*[0-9]).{8,}$";
             //(?=.*[A-Z]) - atleast 1 capital letter
-            Console.WriteLine("\n Password Rules --> 1]Minimum 8 Characters\t2]At least 1 capital alphabet");
-            Console.Write("\n Enter password : ");
-            string password = Console.ReadLine();
+            //(?=.*[0-9]) - atleast 1 number
+            Console.WriteLine("\n Password Rules --> 1]Minimum 8 Characters\t2]At least 1 capital alphabet\t3]At least 1 number");
         }
     }     }
